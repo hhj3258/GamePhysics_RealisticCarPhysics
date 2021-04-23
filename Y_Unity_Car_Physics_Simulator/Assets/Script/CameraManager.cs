172b09cc342a829cfe806724a5cd67117574a48b
Hong Hyeon-Jun <70702088+hhj3258@users.noreply.github.com>
2021-04-24 04:51:08 +0900

    유니티 자동차 시뮬레이션
    프로토타입 업로드

---
+++ Y_Unity_Car_Physics_Simulator/Assets/Script/CameraManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraManager : MonoBehaviour
+{
+    public GameObject focus;
+    public float distance = 3f;
+    public float height = 2f;
+    public float dampening = 0.1f;
+    public float h2 = 0f;
+    public float d2 = 0f;
+    public float l = 0f;
+
+    int camMode = 0;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            camMode = (camMode + 1) % 2;
+        }
+
+        switch (camMode)
+        {
+            case 1:
+                this.transform.position = focus.transform.position + focus.transform.TransformDirection(new Vector3(l, h2, -d2));
+                this.transform.rotation = focus.transform.rotation;
+                Camera.main.fieldOfView = 80f;
+                break;
+
+            default:
+                this.transform.position = Vector3.Lerp(transform.position, focus.transform.position + focus.transform.TransformDirection(new Vector3(0f, height, -distance)), Time.deltaTime * dampening);
+                //this.transform.position = Vector3.Lerp(transform.position, transform.position, Time.deltaTime * dampening);
+                this.transform.LookAt(focus.transform);
+                Camera.main.fieldOfView = 60f;
+                break;
+        }
+        
+
+        //Debug.Log(camMode);
+    }
+}
---
+++ Y_Unity_Car_Physics_Simulator/Assets/Script/InputManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class InputManager : MonoBehaviour
+{
+    public float throttle;
+    public float steer;
+    public bool is_L;
+    public bool brake;
+
+    // Update is called once per frame
+    void Update()
+    {
+        throttle = Input.GetAxis("Vertical");   // W, S
+        //Debug.Log(Input.GetAxis("Vertical"));
+        steer = Input.GetAxis("Horizontal");    //A, D
+
+        is_L = Input.GetKeyDown(KeyCode.L);
+
+        brake = Input.GetKey(KeyCode.Space);
+
+        //Debug.Log(Input.GetAxis("XboxReset"));
+        if (Input.GetKeyDown(KeyCode.R) || (Input.GetAxis("XboxReset") == 1f))
+        {
+            SceneManager.LoadScene(0);
+        }
+
+        
+    }
+}
